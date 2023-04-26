457977e19482b6da084fc68ecbcb07f515f56a44
Dkid555 <dimason2008@gmail.com>
2023-04-26 10:28:53 -0400

    sum of digits

---
+++ algo/sumofdigitstilonedigitinnum.py
@@ -0,0 +1,16 @@
+"""
+11 -> 2
+114 -> 6
+227 -> 11 -> 2
+11111569 -> 25 -> 7
+
+
+"""
+
+
+if __name__ =="__main__":
+    digit = int(input())
+    if digit == 0:
+        print(digit)
+    else:
+        print("it goes till: ", (digit - 1)%9 + 1)
