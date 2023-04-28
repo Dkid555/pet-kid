3643b9dbd617f1331f4fd2523d94c93d8003bb8f
Dkid555 <dimason2008@gmail.com>
2023-04-28 15:10:17 -0400

    looking for longest string in the word, without repeat

---
+++ algo/longestsubstringnorepeat.py
@@ -0,0 +1,19 @@
+def lengthOfLongestSubstring(s: str):
+    if len(s) <= 1:
+        return len(s)
+    d = {}
+    max_s = 0
+    for i in s:
+        if i in d.keys():
+            max_s = max(max_s, len(d))
+            for key in list(d.keys()):
+                d.pop(key)
+                if key == i:
+                    break
+        d[i] = 1
+    max_s = max(max_s, len(d))
+    return max_s
+
+if __name__ == "__main__":
+    s = input()
+    print(lengthOfLongestSubstring(s))
